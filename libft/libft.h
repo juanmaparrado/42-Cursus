e9b6a496766c73c8b7272cbfac4f45990554ccd0
juanmaparrado <juanmanuel.parrado.fp@iescampanillas.com>
2023-09-19 22:36:10 +0200

    Update file.h

--- libft/libft.h
+++ libft/libft.h
@@ -16,6 +16,11 @@
 #include	<unistd.h>;
 #include	<stdlib.h>;
 
-
+ int ft_isalpha(int c);
+ int ft_isdigit(int c);
+ int ft_isalnum(int c);
+ int ft_isascii(int c);
+ int ft_isprint(int c);
+ size_t ft_strlen(const char *s);
 
 #endif
