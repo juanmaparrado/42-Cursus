6682c2ea46cd2ce5b19869a8b869bcb2e50c0b55
Juan Manuel Parrado Escribano <juaparra@c2r5s5.42malaga.com>
2023-09-20 13:46:07 +0200

    strncmp

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juaparra <juaparra@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 13:10:19 by juaparra          #+#    #+#             */
+/*   Updated: 2023/09/20 13:45:05 by juaparra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (str1[i] != '\0' && i < n && str1[i] == str2[i])
+			i++;
+	if (i == n)
+		i -= 1;
+	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: juaparra <juaparra@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 12:06:57 by juaparra          #+#    #+#             */
-/*   Updated: 2023/09/20 13:00:40 by juaparra         ###   ########.fr       */
+/*   Updated: 2023/09/20 13:26:40 by juaparra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,5 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 char	*ft_strchr(char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-
+int		ft_strncmp(const char *str1, const char *str2, size_t n);
 #endif
